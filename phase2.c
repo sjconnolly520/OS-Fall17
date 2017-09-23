1173ebdd428a4bb999a83cbd596299044e42ff9e
sjconnolly520 <sjc2@email.arizona.edu>
2017-09-23 10:25:02 -0700

    First commit. No Significant Changes.

---
+++ phase2.c
@@ -0,0 +1,141 @@
+/* ------------------------------------------------------------------------
+   phase2.c
+
+   University of Arizona
+   Computer Science 452
+
+   ------------------------------------------------------------------------ */
+
+#include "phase1.h"
+#include "phase2.h"
+#include "usloss.h"
+
+#include "message.h"
+#include <string.h>
+#include <stdlib.h>
+
+/* ------------------------- Prototypes ----------------------------------- */
+int start1 (char *);
+extern int start2 (char *);
+int check_kernel_mode(char *);
+int disableInterrupts(void);
+int enableInterrupts(void);
+
+/* -------------------------- Globals ------------------------------------- */
+
+int debugflag2 = 0;
+
+// the mail boxes 
+mailbox MailBoxTable[MAXMBOX];
+
+// also need array of mail slots, array of function ptrs to system call 
+// handlers, ...
+
+
+
+
+/* -------------------------- Functions ----------------------------------- */
+
+/* ------------------------------------------------------------------------
+   Name - start1
+   Purpose - Initializes mailboxes and interrupt vector.
+             Start the phase2 test process.
+   Parameters - one, default arg passed by fork1, not used here.
+   Returns - one to indicate normal quit.
+   Side Effects - lots since it initializes the phase2 data structures.
+   ----------------------------------------------------------------------- */
+int start1(char *arg) {
+    int kid_pid;
+    int status;
+    
+    
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("start1(): at beginning\n");
+
+    check_kernel_mode("start1");    // FIXME: Write method
+    // Disable interrupts
+    disableInterrupts();            // FIXME: Write method
+
+    // Initialize the mail box table, slots, & other data structures.
+    
+    // --- First 7 mailboxes will be assigned to interrupt handlers
+    
+    
+    // Initialize USLOSS_IntVec and system call handlers,
+    
+    // --- Do we want a slot table?
+    // allocate mailboxes for interrupt handlers.  Etc... 
+
+    enableInterrupts();
+
+    // Create a process for start2, then block on a join until start2 quits
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("start1(): fork'ing start2 process\n");
+    kid_pid = fork1("start2", start2, NULL, 4 * USLOSS_MIN_STACK, 1);
+    if ( join(&status) != kid_pid ) {
+        USLOSS_Console("start2(): join returned something other than ");
+        USLOSS_Console("start2's pid\n");
+    }
+
+    return 0;
+} /* start1 */
+
+
+/* ------------------------------------------------------------------------
+   Name - MboxCreate
+   Purpose - gets a free mailbox from the table of mailboxes and initializes it 
+   Parameters - maximum number of slots in the mailbox and the max size of a msg
+                sent to the mailbox.
+   Returns - -1 to indicate that no mailbox was created, or a value >= 0 as the
+             mailbox id.
+   Side Effects - initializes one element of the mail box array. 
+   ----------------------------------------------------------------------- */
+int MboxCreate(int slots, int slot_size) {
+    
+    // Check kernelMode
+    // Disable Interrupts
+    
+    // Error Checking...
+        // slots < 0 ---> return -1
+        // slot_size < 0 ---> return -1
+        // slot_size > MAX_MESSAGE ---> -1
+    
+    // Initialize Mailbox
+        // What parameters need initializing?
+    
+    // Enable interrupts
+    
+    // FIXME: IF MAILBOX FAILED TO CREATE ---> Return -1 (Did he say this in class?)
+    
+    // Return index of new mailbox in MailBoxTable
+    return -404;
+} /* MboxCreate */
+
+
+/* ------------------------------------------------------------------------
+   Name - MboxSend
+   Purpose - Put a message into a slot for the indicated mailbox.
+             Block the sending process if no slot available.
+   Parameters - mailbox id, pointer to data of msg, # of bytes in msg.
+   Returns - zero if successful, -1 if invalid args.
+   Side Effects - none.
+   ----------------------------------------------------------------------- */
+int MboxSend(int mbox_id, void *msg_ptr, int msg_size) {
+    
+    return -404;
+} /* MboxSend */
+
+
+/* ------------------------------------------------------------------------
+   Name - MboxReceive
+   Purpose - Get a msg from a slot of the indicated mailbox.
+             Block the receiving process if no msg available.
+   Parameters - mailbox id, pointer to put data of msg, max # of bytes that
+                can be received.
+   Returns - actual size of msg if successful, -1 if invalid args.
+   Side Effects - none.
+   ----------------------------------------------------------------------- */
+int MboxReceive(int mbox_id, void *msg_ptr, int msg_size) {
+    
+    return -404;
+} /* MboxReceive */
